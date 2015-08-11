d5f845f0b3980549ff8fe771b9243642c6b654f5
Eric Prates <itseric@mail.ru>
2015-08-10 22:45:12 -0300

    Cleaned and restarted old project as a NetBeans Platform Application.
    Connection to the database is functional. Courses are displayed in a
    BeanTreeView component and their properties are accessible.

---
+++ appViewer/build/classes-generated/org/timetable/viewer/Bundle.java
@@ -0,0 +1,27 @@
+package org.timetable.viewer;
+/** Localizable strings for {@link org.timetable.viewer}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>appViewer</i>
+     * @see appViewerTopComponent
+     */
+    static String CTL_appViewerAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_appViewerAction");
+    }
+    /**
+     * @return <i>appViewer Window</i>
+     * @see appViewerTopComponent
+     */
+    static String CTL_appViewerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_appViewerTopComponent");
+    }
+    /**
+     * @return <i>This is a appViewer window</i>
+     * @see appViewerTopComponent
+     */
+    static String HINT_appViewerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_appViewerTopComponent");
+    }
+    private void Bundle() {}
+}
---
+++ appViewer/src/org/timetable/viewer/CursoChildFactory.java
@@ -0,0 +1,44 @@
+/*
+ */
+package org.timetable.viewer;
+
+import appModelLibrary.Curso;
+import java.beans.IntrospectionException;
+import java.util.List;
+import org.openide.nodes.BeanNode;
+import org.openide.nodes.ChildFactory;
+import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author Eric <itseric@mail.ru>
+ */
+public class CursoChildFactory extends ChildFactory<Curso>{
+
+    private List<Curso> resultList;
+
+    public CursoChildFactory(List<Curso> resultList) {
+        this.resultList = resultList;
+    }
+
+    @Override
+    protected boolean createKeys(List<Curso> list) {
+        for (Curso curso : resultList) {
+            list.add(curso);
+        }
+        return true;
+    }
+
+    @Override
+    protected Node createNodeForKey(Curso c) {
+        try {
+            return new BeanNode(c);
+        } catch (IntrospectionException ex) {
+            Exceptions.printStackTrace(ex);
+            return null;
+        }
+    }
+
+    
+}
